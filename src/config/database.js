7da5b2bd7afd6d4462c9be71e934aeed6114ef05
Benja <encinabenja750@gmail.com>
2026-08-28 10:17:22 -0300

    Se estableció conexión a la base de datos

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize("task_users_db", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+export const testdb = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({ force: false });
+    console.log("Conexión a la base de datos exitosa.");
+  } catch (error) {
+    console.error("No se pudo conectar a la base de datos", error);
+  }
+};
