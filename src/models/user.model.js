4dd6c04e40f51aecc73487156c4dfd6f26d98834
Benja <encinabenja750@gmail.com>
2026-08-28 10:36:12 -0300

    Se creó el modelo de usuario

---
+++ src/models/user.model.js
@@ -0,0 +1,18 @@
+import { sequelize } from "../config/database.js";
+import { DataTypes } from "sequelize";
+
+export const User = sequelize.define("User", {
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+});
