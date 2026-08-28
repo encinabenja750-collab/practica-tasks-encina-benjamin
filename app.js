6d7a00b7be07b23fc54e8878e2f8855cdeb561b9
Benja <encinabenja750@gmail.com>
2026-08-28 10:24:31 -0300

    Se añadió una función para la conexión en app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import { testdb } from "./src/config/database.js";
 
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
+  testdb();
   console.log(`Servidor ejecutandose en el puerto ${PORT}`);
 });
