224574a3ec1bd206fdd7d74928a0aeaef0abd67d
Benja <encinabenja750@gmail.com>
2026-08-30 19:29:11 -0300

    Se completó el CRUD de usuarios

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -60,3 +60,104 @@ export const crearUsuario = async (req, res) => {
     });
   }
 };
+
+export const obtenerUsuarioId = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const usuario = await User.findByPk(id);
+    if (!usuario) {
+      return res
+        .status(404)
+        .json({ message: "El usuario solicitado no existe." });
+    }
+    return res.status(200).json({ data: usuario });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Ocurrió un error al buscar el usuario",
+      error: error.message,
+    });
+  }
+};
+
+export const actualizarUsuarioId = async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.boy;
+
+  if (name !== undefined && (name.trirm() === "" || name.lenght > 100)) {
+    return res.status(400).json({
+      message: "El nombre no puede estar vacío ni ser mayor a 100 carácteres",
+    });
+  }
+  if (email !== undefined && (email.trim() === "" || email.lenght > 100)) {
+    return res.status(400).json({
+      message:
+        "El correo eléctronico no puede estar vacío ni ser mayor a 100 carácteres",
+    });
+  }
+  if (
+    password !== undefined &&
+    (password.trim() === "" || password.lenght > 100)
+  ) {
+    return res.status(400).json({
+      message:
+        "La contraseña no puede estar vacía ni ser mayor a 100 carácteres",
+    });
+  }
+  try {
+    const usuario = await User.findByPk(id);
+    if (!usuario) {
+      return res
+        .status(404)
+        .json({ message: "El usuario que intenta actualizar no existe" });
+    }
+    if (email && email.trim() !== usuario.email.trim()) {
+      const emailRepetido = await User.findOne({
+        where: { email: email.trim() },
+      });
+      if (emailRepetido) {
+        return res
+          .status(400)
+          .json({ message: "El correo electrónico ya está registrado" });
+      }
+    }
+    usuario.name = name !== undefined ? name : usuario.name;
+    usuario.email = email !== undefined ? email : usuario.email;
+    usuario.password = password !== undefined ? password : usuario.password;
+
+    await usuario.save();
+
+    return res.status(200).json({
+      message: "Usuario actualizado con éxito",
+      data: usuario,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Ocurrió un error al actualizar el usuario",
+      error: error.message,
+    });
+  }
+};
+
+export const eliminarUsuario = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const usuario = await User.findByPk(id);
+    if (!usuario) {
+      return res.status(404).json({
+        message: "No se puede eliminar porque el usuario no existe",
+      });
+    }
+    await usuario.destroy();
+    return res.status(200).json({
+      message: "Usuario eliminado con éxito",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Ocurrió un error al intentar elminar el usuario",
+      error: error.message,
+    });
+  }
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,9 +2,15 @@ import { Router } from "express";
 import {
   crearUsuario,
   obtenerUsuarios,
+  obtenerUsuarioId,
+  actualizarUsuarioId,
+  eliminarUsuario,
 } from "../controllers/user.controllers.js";
 
 export const usersruta = Router();
 
 usersruta.get("/api/users", obtenerUsuarios);
 usersruta.post("/api/users", crearUsuario);
+usersruta.get("/api/users/:id", obtenerUsuarioId);
+usersruta.put("/api/users/:id", actualizarUsuarioId);
+usersruta.delete("/api/users/:id", eliminarUsuario);
